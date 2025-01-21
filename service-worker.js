71b9adc4c2535538f341c49ff2a381416e0fbb68
Farouk Afolabi <farouk.afolabi@yahoo.com>
2025-01-21 18:51:08 -0500

    made an icons folder

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,12 @@
 const CACHE_NAME = 'to-do-pwa-cache-v1';
 const FILES_TO_CACHE = [
- '/YOUR-REPOSITORY-NAME/',
- '/YOUR-REPOSITORY-NAME /index.html',
- '/YOUR-REPOSITORY-NAME /style.css',
- '/YOUR-REPOSITORY-NAME /app.js',
- '/YOUR-REPOSITORY-NAME /manifest.json',
- '/YOUR-REPOSITORY-NAME /icons/icon-128.png',
- '/YOUR-REPOSITORY-NAME /icons/icon-512.png'
+ '/CheckList-Sec01/',
+ '/CheckList-Sec01 /index.html',
+ '/CheckList-Sec01 /style.css',
+ '/CheckList-Sec01 /app.js',
+ '/CheckList-Sec01 /manifest.json',
+ '/CheckList-Sec01 /icons/icon-128.png',
+ '/CheckList-Sec01 /icons/icon-512.png'
 ];
 self.addEventListener('install', (event) => {
  event.waitUntil(
